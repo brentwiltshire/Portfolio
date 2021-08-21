2b241956c5d5ff0728b180eb5e65a1239b186755
brentwiltshire <brentwiltshire60@hotmail.com>
2021-08-21 13:59:36 +1000

    Formatted script and removed unneccessary echos

--- Week_4/IpAddressesOnly.sh
+++ Week_4/IpAddressesOnly.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#Running other code  
+# Intent is to display formatted text from body of system information text.
+
+# Running IpInfo.sh script  
 source /home/brent/Student/Scripts/Portfolio/Week_4/IpInfo.sh
 
-#echo -e "IP addresses on this computer are:\n$addresses" 
-
-#Trying to get Use sed to display only the lines with 'IP Address' on them
-
-#Address_info="$addresses"
-#echo "RUNNN $Address_info"
-
+# Use of SED to display only the lines with 'IP Address' on them
 IP_addresses=$(echo "$addresses" | sed -n '/IP Address/ {
     s/IP address/""/
     p
-    }')
+}')
 
 echo -e "Displaying just IP addresses of network address: \n$IP_addresses"
+exit 0
+
+# REFERENCES
+#   https://linuxize.com/post/how-to-use-sed-to-find-and-replace-string-in-files/
